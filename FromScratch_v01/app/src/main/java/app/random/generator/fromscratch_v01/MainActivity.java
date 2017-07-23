0ddf9cb1acd5197f484e4a893ecfa03c6696fc4d
AnaliMoreano <analimoreano10@gmail.com>
2017-07-23 12:27:18 -0500

    Correccion de primera version

---
+++ FromScratch_v01/app/src/main/java/app/random/generator/fromscratch_v01/MainActivity.java
@@ -0,0 +1,15 @@
+package app.random.generator.fromscratch_v01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.anali.fromscratch_v01.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
