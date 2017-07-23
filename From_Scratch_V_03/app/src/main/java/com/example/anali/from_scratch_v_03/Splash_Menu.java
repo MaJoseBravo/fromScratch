f6df4f74677f800b35b43bbc457380d205f343ac
AnaliMoreano <analimoreano10@gmail.com>
2017-07-23 16:56:04 -0500

    Tercera version P1

---
+++ From_Scratch_V_03/app/src/main/java/com/example/anali/from_scratch_v_03/Splash_Menu.java
@@ -0,0 +1,48 @@
+package com.example.anali.from_scratch_v_03;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class Splash_Menu extends AppCompatActivity {
+
+    int progress = 0;
+    ProgressBar horizontalProgressBar;
+    Handler h = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__menu);
+
+        horizontalProgressBar = (ProgressBar)findViewById(R.id.progressBarHome);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<5; i++){
+                    progress+=50;
+                    h.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            horizontalProgressBar.setProgress(progress);
+                            if (progress == horizontalProgressBar.getMax()){
+                                //horizontalProgressBar.setVisibility(View.VISIBLE);
+                                //Intent intent = new Intent(getApplicationContext(), Menu_Drawer.class);
+                                //startActivity(intent);
+                                startActivity(new Intent(Splash_Menu.this, MainActivity.class));
+                            }
+                        }
+                    });
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e){
+
+                    }
+                }
+            }
+        }).start();
+    }
+}
